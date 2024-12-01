e5602633cd5ffd78e6e7073ea836dbb95d719d24
Mmaduabuchi Elvis Igwilo <148869665+Elhefeelvis1@users.noreply.github.com>
2024-12-01 20:32:27 +0100

    small update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ejs from 'ejs';
 import axios from "axios";
 
 const app = express();
-const port = process.emv.PORT;
+const port = 3000;
 const apiKey = "9e2653682c0cd4575d081fb8763a8514"
 
 app.use(express.static("public"));
